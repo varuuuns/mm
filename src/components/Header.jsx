180fa1ba894dc4cee9fa836b07ed8c28ee77bb10
VARUN S <varuuun.s@gmail.com>
2025-12-08 21:22:54 +0530

    comp:hader

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header() {
     return (
         <header className="header">
-            <h1 className="title">Conquer The Challenge</h1>
+            <h1 className="title">Course Finder</h1>
             <p className="subtitle">
-                Web Developer Intern Assignment
+                Find the right course for you
             </p>
         </header>
     )
